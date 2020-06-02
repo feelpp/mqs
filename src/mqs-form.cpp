a34efa11916c63b4d63622d064e478a19608c325
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2020-06-02 09:14:52 +0000

    Add mqs with blockmatrix (see blocklaplacian ex) Add support for model-file

--- src/mqs-form.cpp
+++ src/mqs-form.cpp
@@ -77,17 +77,40 @@ int main(int argc, char**argv )
   // Define SpaceFunctions
   auto Ah = Pchv<1>( mesh );
   auto Vh = Pch<1>( mesh, markedelements(mesh, range) );
-  auto cond_mesh = Vh->mesh();
-  
-  // Space Product
-  auto Zh = product(Ah,Vh);
-  auto U = Zh.element();
 
+  auto cond_mesh = Vh->mesh();
+  if (Environment::worldComm().isMasterRank())
+    {
+      std::cout << "mesh->numGlobalElements() "<< mesh->numGlobalElements() << std::endl;
+      std::cout << "cond_mesh->numGlobalElements() "<< cond_mesh->numGlobalElements() << std::endl;
+      std::cout << "Ah->nDof() "<<Ah->nDof() << std::endl;
+      std::cout << "Vh->nDof() "<<Vh->nDof() << std::endl;
+    }
+
+  auto A = Ah->elementPtr(); //Ah->element(A0); // how to init A to A0?;
+  auto V = Vh->elementPtr(); //Vh->element(V0);
+
+  // Vincent way
   tic();
-  solve::strategy strategy = solve::strategy::monolithic; // if it enough if I want use fieldsplit?
-  auto rhs = blockform1( Zh, strategy ,backend(_name="mqs") );
-  auto lhs = blockform2( Zh, strategy ,backend(_name="mqs") );
+  BlocksBaseGraphCSR myblockGraph(2,2);
+  myblockGraph(0,0) = stencil(_test=Ah,_trial=Ah, _diag_is_nonzero=false, _close=false)->graph();
+  myblockGraph(0,1) = stencil(_test=Ah,_trial=Vh, _diag_is_nonzero=false, _close=false)->graph();
+  myblockGraph(1,0) = stencil(_test=Vh,_trial=Ah, _diag_is_nonzero=false, _close=false)->graph();
+  myblockGraph(1,1) = stencil(_test=Vh,_trial=Vh, _diag_is_nonzero=false, _close=false)->graph();
+  auto M = backend()->newBlockMatrix(_block=myblockGraph);
+
+  BlocksBaseVector<double> myblockVec(2);
+  myblockVec(0,0) = backend()->newVector( Ah );
+  myblockVec(1,0) = backend()->newVector( Vh );
+  auto F = backend()->newBlockVector(_block=myblockVec, _copy_values=false);
+
+  BlocksBaseVector<double> myblockVecSol(2);
+  myblockVecSol(0,0) = A;
+  myblockVecSol(1,0) = V;
+  auto U = backend()->newBlockVector(_block=myblockVecSol, _copy_values=false);
   toc("create blockforms", true);
+
+  auto mybackend = backend(_name="mqs");
 
   double t = 0;
 
@@ -97,8 +120,6 @@ int main(int argc, char**argv )
   auto Aexact = Ah->element();
   auto Vexact = Vh->element();
 
-  auto A = Ah->element(); //Ah->element(A0); // how to init A to A0?;
-  auto V = Vh->element(); //Vh->element(V0);
   if ( Uexact )
     {
       Aexact_g.setParameterValues({{"t", t}});
@@ -109,23 +130,20 @@ int main(int argc, char**argv )
       Vexact = project(_space = Vh, _expr = Vexact_g);
       Feel::cout << "Define Vexact" << std::endl;
   
-      A = Aexact;
-      V = Vexact;
+      (*A) = Aexact;
+      (*V) = Vexact;
     }
   toc("init exact solution", true);
 
-  auto phi = Ah->element();
-  auto psi = Vh->element();
-  
   
 #if 1
   node_type pt(3);
   pt[0] = 0.5; pt[1] = 0.5; pt[2] = 2.5;
-  auto val = A(pt);
+  auto val = (*A)(pt);
   auto Ax = val(0,0,0); // evaluation de Ay
   auto Ay = val(1,0,0); // evaluation de Az
   auto Az = val(2,0,0); // evaluation de Az
-  auto Vval = V(pt);
+  auto Vval = (*V)(pt);
   Feel::cout << "t=" << t << ", ";
   Feel::cout << "A(" << pt[0] << "," << pt[1] << "," << pt[2] << ") = {" << Ax << "," << Ay << "," << Az << "}, ";
   Feel::cout << "V(" << pt[0] << "," << pt[1] << "," << pt[2] << ")=" << Vval(0,0,0) << std::endl;
@@ -155,7 +173,7 @@ int main(int argc, char**argv )
       L2Verror = 0;
     }
   
-  auto mu0 = 4.e-7 * M_PI ; // SI Unit : H/m = m.kg/s2/A2
+  auto mu0 = 1; // 4.e-7 * M_PI ; // SI Unit : H/m = m.kg/s2/A2
 
   
   for (t = dt; t < tmax; t += dt)
@@ -170,8 +188,9 @@ int main(int argc, char**argv )
 	  auto mur = material.getScalar("mu_mag");
 
 	  // Ampere law: sigma dA/dt + rot(1/(mu_r*mu_0) rotA) + sigma grad(V) = Js
-	  lhs(0_c, 0_c) += integrate( _range=markedelements(mesh, material.meshMarkers()),
-				      _expr = dt * 1/mur * inner(curl(phi) , curlt(A)) );
+	  form2( _trial=Ah, _test=Ah ,_matrix=M )
+	    += integrate( _range=markedelements(mesh, material.meshMarkers()),
+			  _expr = dt * 1/mur * inner(curl(A) , curlt(A)) );
 	  Feel::cout << "create lhs(0,0)" << std::endl;
 
 	}
@@ -184,30 +203,54 @@ int main(int argc, char**argv )
 	  auto sigma = material.getScalar("sigma");
 
 	  // Ampere law: sigma dA/dt + rot(1/(mu_r*mu_0) rotA) + sigma grad(V) = Js
-	  lhs(0_c, 0_c) += integrate( _range=markedelements(cond_mesh, material.meshMarkers()),
-				      _expr = mu0 * sigma * inner(id(phi) , idt(A) ));
+	  form2( _trial=Ah, _test=Ah ,_matrix=M )
+	    += integrate( _range=markedelements(cond_mesh, material.meshMarkers()),
+			  _expr = mu0 * sigma * inner(id(A) , idt(A) ));
 	  Feel::cout << "create lhs(0,0)" << std::endl;
 
-	  lhs(0_c, 1_c) += integrate(_range=markedelements(cond_mesh, material.meshMarkers()),
-				     _expr = dt * mu0 * sigma * inner(id(phi),trans(gradt(V))) );
+	  form2( _trial=Vh, _test=Ah ,_matrix=M, _rowstart=0, _colstart=1 )
+	    += integrate(_range=markedelements(cond_mesh, material.meshMarkers()),
+			 _expr = dt * mu0 * sigma * inner(id(A),trans(gradt(V))) );
 	  Feel::cout << "create lhs(0,1)" << std::endl;
 
-	  rhs(0_c) += integrate(_range=markedelements(cond_mesh, material.meshMarkers()),
-				_expr = mu0 * sigma * inner(id(phi) , idv(A)));
-	  Feel::cout << "create row(0)" << std::endl;
+	  form1( _test=Ah, _vector=F )
+	    += integrate(_range=markedelements(cond_mesh, material.meshMarkers()),
+			 _expr = mu0 * sigma * inner(id(A) , idv(A)));
+	  Feel::cout << "create rhs(0)" << std::endl;
+
+	  // auto Js = ;
+	  // form1( _test=Ah, _vector=F )
+	  //   += integrate(_range=markedelements(cond_mesh, material.meshMarkers()),
+	  // 		 _expr = dt * mu0 * inner(id(A) , Js));
+	  // Feel::cout << "create rhs(0)" << std::endl;
 
 	  // Current conservation: div( -sigma grad(V) -sigma*dA/dt) = Qs
-	  lhs(1_c, 1_c) += integrate( _range=markedelements(cond_mesh, material.meshMarkers()),
-				      _expr = sigma * dt * inner(gradt(V), grad(psi)) );
+	  form2( _trial=Vh, _test=Vh ,_matrix=M, _rowstart=1, _colstart=1 )
+	    += integrate( _range=markedelements(cond_mesh, material.meshMarkers()),
+			  _expr = sigma * dt * inner(gradt(V), grad(V)) );
 	  Feel::cout << "create lhs(1,1)" << std::endl;
-	  lhs(1_c, 0_c) += integrate( _range=markedelements(cond_mesh, material.meshMarkers()),
-				      _expr = sigma * inner(idt(A), trans(grad(psi))) );
-	  Feel::cout << "create row(1)" << std::endl;
+
+	  form2( _trial=Ah, _test=Vh ,_matrix=M, _rowstart=1, _colstart=0 )
+	    += integrate( _range=markedelements(cond_mesh, material.meshMarkers()),
+			  _expr = sigma * inner(idt(A), trans(grad(V))) );
+	  Feel::cout << "create lhs(1,0)" << std::endl;
+
+	  form1( _test=Vh ,_vector=F, _rowstart=1 )
+	    += integrate( _range=markedelements(cond_mesh, material.meshMarkers()),
+			  _expr = sigma * inner(idt(A), trans(grad(V))) );
+	  Feel::cout << "create rhs(1)" << std::endl;
+
+	  // auto Qs = ...;
+	  // form1( _test=Vh, _vector=F, _rowstart=1 )
+	  //   += integrate(_range=markedelements(cond_mesh, material.meshMarkers()),
+	  // 		 _expr = dt * Qs * id(V);
+	  // Feel::cout << "create row(1)" << std::endl;
 	}
       toc("assembling", true);
      
       tic();
-      // define M_weakdir
+      // Implement Dirichlet fort
+      // TODO: define M_weakdir
       auto itField = M_modelProps->boundaryConditions().find( "magnetic-potential");
       if ( itField != M_modelProps->boundaryConditions().end() )
 	{
@@ -220,9 +263,9 @@ int main(int argc, char**argv )
 		  std::string marker = exAtMarker.marker();
 		  auto g = expr<3,1>(exAtMarker.expression());
 		  g.setParameterValues({{"t", t}});
-		  Feel::cout << "Dirichlet[" << marker << "] : " << exAtMarker.expression() << std::endl;
-		  lhs(0_c, 0_c) += on(_range=markedfaces(mesh,marker), _rhs=rhs(0_c), _element=phi, _expr= g);
-		  Feel::cout << "block(0,0) on " << marker << std::endl;
+		  Feel::cout << "A Dirichlet[" << marker << "] : " << exAtMarker.expression() << ", g=" << g << std::endl;
+		  form2( _trial=Ah, _test=Ah ,_matrix=M, _rowstart=0, _colstart=0 )
+		    += on(_range=markedfaces(mesh,marker), _rhs=F, _element=*A, _expr= g);
 		}
 	    }
 	  // 	ItType = mapField.find( "Neumann" );
@@ -251,9 +294,9 @@ int main(int argc, char**argv )
 		  std::string marker = exAtMarker.marker();
 		  auto g = expr(exAtMarker.expression());
 		  g.setParameterValues({{"t", t}});
-		  Feel::cout << "Dirichlet[" << marker << "] : " << exAtMarker.expression() << std::endl;
-		  lhs(1_c, 1_c) += on(_range=markedfaces(cond_mesh,marker), _rhs=rhs(1_c), _element=psi, _expr= g);
-		  Feel::cout << "block(1,1) on " << marker << std::endl;
+		  Feel::cout << "V Dirichlet[" << marker << "] : " << exAtMarker.expression() << ", g=" << g << std::endl;
+		  form2( _trial=Vh, _test=Vh ,_matrix=M, _rowstart=1, _colstart=1 )
+		    += on(_range=markedfaces(cond_mesh,marker), _rhs=F, _element=*V, _expr= g);
 		}
 	    }
 	}       
@@ -261,26 +304,24 @@ int main(int argc, char**argv )
     
       /* Solve */
       tic();
-      auto result = lhs.solve(_rhs=rhs,_solution=U);
+      auto result = mybackend->solve( _matrix=M, _rhs=F, _solution=U);
       toc("solve", true);
 
+      myblockVecSol.localize(U);
 #if 1
-      A = U(0_c); 
-      V = U(1_c);
-
-      val = A(pt);
+      val = (*A)(pt);
       Ax = val(0,0,0); // evaluation de Ay
       Ay = val(1,0,0); // evaluation de Az
       Az = val(2,0,0); // evaluation de Az
-      Vval = V(pt);
+      Vval = (*V)(pt);
       Feel::cout << "t=" << t << ", ";
       Feel::cout << "A(" << pt[0] << "," << pt[1] << "," << pt[2] << ") = {" << Ax << "," << Ay << "," << Az << "}, ";
       Feel::cout << "V(" << pt[0] << "," << pt[1] << "," << pt[2] << ")=" << Vval(0,0,0) << std::endl;
 #endif
 
       tic();
-      e->step(t)->add( "A", U(0_c));
-      e->step(t)->add( "V", U(1_c));
+      e->step(t)->add( "A", A);
+      e->step(t)->add( "V", V);
     
       if ( Uexact )
 	{
@@ -299,20 +340,31 @@ int main(int argc, char**argv )
       if ( Uexact )
 	{
 	  L2Aexact = normL2(_range = elements(mesh), _expr = Aexact_g);
+	  L2Aerror = normL2(elements(mesh), (idv(A) - idv(Aexact)));
+	  H1Aerror = normH1(elements(mesh), _expr = (idv(A) - idv(Aexact)), _grad_expr = (gradv(A) - gradv(Aexact)));
+#if 0
 	  L2Aerror = normL2(elements(mesh), (idv(U(0_c)) - idv(Aexact)));
 	  H1Aerror = normH1(elements(mesh), _expr = (idv(U(0_c)) - idv(Aexact)), _grad_expr = (gradv(U(0_c)) - gradv(Aexact)));
-
-	  Feel::cout << "A error: " << "t="<< t << " " << L2Aerror << " " << L2Aerror / L2Aexact << " " << H1Aerror << std::endl;
+#endif
+	  Feel::cout << "error: " << "t="<< t;
+	  Feel::cout << " A: " << L2Aerror << " " << L2Aerror / L2Aexact << " " << H1Aerror << " ";
 
 	  L2Vexact = normL2(_range = elements(cond_mesh), _expr = Vexact_g);
+	  L2Verror = normL2(elements(cond_mesh), (idv(V) - idv(Vexact)));
+	  H1Verror = normH1(elements(cond_mesh), _expr = (idv(V) - idv(Vexact)), _grad_expr = (gradv(V) - gradv(Vexact)));
+#if 0
 	  L2Verror = normL2(elements(cond_mesh), (idv(U(1_c)) - idv(Vexact)));
 	  H1Verror = normH1(elements(cond_mesh), _expr = (idv(U(1_c)) - idv(Vexact)), _grad_expr = (gradv(U(1_c)) - gradv(Vexact)));
-
-	  Feel::cout << "V error: " << "t="<< t << " " << L2Verror << " " << L2Verror / L2Vexact << " " << H1Verror << std::endl;
+#endif
+	  Feel::cout << " V: " << L2Verror << " " << L2Verror / L2Vexact << " " << H1Verror << std::endl;
 	}
     
       /* reinit  */
+      M->zero();
+      F->zero();
+#if 0
       lhs.zero();
       rhs.zero();
+#endif
     }
 }
